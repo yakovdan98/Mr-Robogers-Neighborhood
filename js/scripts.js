c04f5c25871ac501cbbb8b458102922d3d14e9b9
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-10-28 09:16:11 -0700

    wrote tests and implemented outline

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//Business Logic
+function beepBoop(number) {
+  const arrayNums = [];
+  for (let i = 0; i < number; i++) {
+    arrayNums.push(i);
+  }
+  return arrayNums;
+}
+
+
+//UI logic
+window.addEventListener("load", function () {
+
+});
