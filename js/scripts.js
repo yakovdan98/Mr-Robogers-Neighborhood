07903d5ad5ab3ae95f9e13b77f2fccf05f1f7ae4
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-10-28 09:38:28 -0700

    added beep functionality

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,19 @@
 function beepBoop(number) {
   const arrayNums = [];
   for (let i = 0; i < number; i++) {
-    arrayNums.push(i);
+    let num = i.toString();
+    let numInserted = false;
+    for(let j = 0; j < num.length;j++){
+      if(num[j] === "1"){
+        arrayNums.push("Beep!");
+        numInserted = true;
+        break;
+      }
+    }
+    if(!numInserted){
+      arrayNums.push(i);
+    }
+    
   }
   return arrayNums;
 }
