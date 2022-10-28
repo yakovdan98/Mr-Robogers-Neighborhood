0e6ea3b3af2f6108112d6b362f2cd7fd42779da6
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-10-28 10:24:34 -0700

    added full beepBoop functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,31 @@
 //Business Logic
+
+
 function beepBoop(number) {
   const arrayNums = [];
-  for (let i = 0; i < number; i++) {
+  for (let i = 0; i <= number; i++) {
     let num = i.toString();
-    let type = 0; //1 === Beep!, 2 === Boop!
+    let type = 0; //1 === Beep!, 2 === Boop!, 3 === Won't you be my neighbor?
     for(let j = 0; j < num.length;j++){
-      if (num[j] === "2"){
-        type = 2;
+      if (num[j] === "3"){
+        type = 3;
         break;
       }
-      else if(num[j] === "1" && type !== 2){
+      else if (num[j] === "2" && type < 2){
+        type = 2;
+      }
+      else if(num[j] === "1" && type < 1 ){
         type = 1;
       }
     }
     if(type === 1){ //insert Beep!
       arrayNums.push("Beep!");
     }
-    else if(type === 2){
+    else if(type === 2){//insert Boop!
       arrayNums.push("Boop!");
+    }
+    else if(type === 3){//insert Won't you be my neighbor?
+      arrayNums.push("Won't you be my neighbor?");
     }
     else{
       arrayNums.push(i);
@@ -30,5 +38,5 @@ function beepBoop(number) {
 
 //UI logic
 window.addEventListener("load", function () {
-
+  
 });
